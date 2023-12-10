b24c8ec25d9ea4ee1a8ac82b296e427c005a85ed
Stark <mattstark08@gmail.com>
2023-12-10 16:11:39 -0600

    faq formatting

--- client/pages/RulesPage.jsx
+++ client/pages/RulesPage.jsx
@@ -1,20 +1,32 @@
 export default function RulesPage() {
 
     return (
-      <>
-       <h1>Game Rules & FAQ</h1>
+        <>
+            <h1>Game Rules & FAQ</h1>
 
-       <h2>Game Rules:</h2>
+            <div>
+                <h2>Game Rules:</h2>
 
-        <div>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quam corrupti eveniet facere deleniti ducimus aperiam tenetur optio similique fuga hic velit doloribus eaque nostrum itaque, beatae harum error alias sit? </div>
+                <div>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quam corrupti eveniet facere deleniti ducimus aperiam tenetur optio similique fuga hic velit doloribus eaque nostrum itaque, beatae harum error alias sit? </div>
+            </div>
 
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure dicta non, corporis quam nulla totam rerum sunt quas repellendus suscipit, eligendi expedita sequi illo, laboriosam dolore sint dolorem fugiat. Autem?</div>
+            <h2> FAQ: </h2>
 
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure dicta non, corporis quam nulla totam rerum sunt quas repellendus suscipit, eligendi expedita sequi illo, laboriosam dolore sint dolorem fugiat. Autem?</div>
+            <ul>
 
-        <h2> FAQ: </h2>
-
-        <div> what?</div>
-      </>
+                <li>
+                    <b> what is?</b>
+                    <i> a tactical grid based card game </i>
+                </li>
+                <li>
+                    <b> is Gary a martian?</b>
+                    <i> idk, probably </i>
+                </li>
+                <li>
+                    <b> is Dizzy a dog?</b>
+                    <i> decended from wolves </i>
+                </li>
+            </ul>
+        </>
     )
-  }
+}
